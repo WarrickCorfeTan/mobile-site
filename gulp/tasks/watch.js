fc94d1bb5525da0378472638e147bd318ebf6149
warrickct <warrickct@gmail.com>
2017-08-27 21:53:50 +1200

    Separated gulp file tasks, made mobile first adaptations to hero class elements, created BEM concepts to CSS structure added additional dependencies

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function(){
+
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"
+		}
+	});
+
+	watch('./app/index.html', function(){
+		browserSync.reload();
+	});
+
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('cssInject');
+	});
+});
+
+//exectures arg[1] "dependency task" then task arg[0] then arg[2]
+gulp.task('cssInject', ['styles'], function() {
+	return gulp.src('./app/temp/styles/styles.css')
+	.pipe(browserSync.stream());
+});
